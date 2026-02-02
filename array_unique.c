76500a1ebc18a7e97d197c3cf625ea44aca6c6be
sai076285-hash <sai076285@gmail.com>
2026-02-02 15:44:59 +0530

    Create array_unique.c

---
+++ array_unique.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int n, i, j, count = 0;
+    printf("Enter size: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    for(i = 0; i < n; i++) {
+        for(j = i + 1; j < n; j++) {
+            if(arr[i] == arr[j]) {
+                count++;
+                break;
+            }
+        }
+    }
+    printf("Total duplicate elements = %d", count);
+    return 0;
+}
